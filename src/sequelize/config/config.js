cb6ab4afa49d25b4675ac4e18777208d00170b3c
nato-re <alvares.renato@hotmail.com>
2021-10-22 14:35:44 -0300

    test action

--- src/sequelize/config/config.js
+++ src/sequelize/config/config.js
@@ -4,20 +4,20 @@ module.exports = {
     password: null,
     database: 'database_development',
     host: '127.0.0.1',
-    dialect: 'mysql',
+    dialect: 'postgres',
   },
   test: {
     username: 'root',
     password: null,
     database: 'database_test',
     host: '127.0.0.1',
-    dialect: 'mysql',
+    dialect: 'postgres',
   },
   production: {
     username: 'root',
     password: null,
     database: 'database_test',
     host: '127.0.0.1',
-    dialect: 'mysql',
+    dialect: 'postgres',
   },
 };
