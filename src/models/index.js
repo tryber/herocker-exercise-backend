66dc66004c773b5521e272a61b847f7d488cdc1b
nato-re <alvares.renato@hotmail.com>
2021-10-22 14:34:03 -0300

    test action

--- src/models/index.js
+++ src/models/index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require('../sequelize/config/config.js')[env];
+const config = require(path.resolve('src/sequelize/config/', 'config.js'))[env];
 const db = {};
 
 let sequelize;
