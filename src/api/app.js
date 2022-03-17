ec604c2a0202d84f51b396cf98af8ae1f8122c42
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-03-17 16:09:54 -0400

    Going up back-end heroku container

--- src/api/app.js
+++ src/api/app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   next();
-})
+});
 
 app.get('/users', rescue(UserController));
 app.use(errorMiddleware);
