005cda1b94cc69db1196b288711e350cfe051f81
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-03-17 16:55:15 -0400

    Try to going up back-end

--- src/api/app.js
+++ src/api/app.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const rescue = require('express-rescue');
+const cors = require('cors');
 const UserController = require('../controllers/user');
 const errorMiddleware = require('../middlewares/error');
 
 const app = express();
+
+var corsOptions = {
+  origin: 'db.edvhklrmxfcucohyylun.supabase.co',
+};
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   next();
 });
 
-app.get('/users', rescue(UserController));
+app.get('/users', cors(corsOptions), rescue(UserController));
 app.use(errorMiddleware);
 
 module.exports = app;
