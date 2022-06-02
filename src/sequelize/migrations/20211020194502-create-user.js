7c44070c0b374b315e7f8c01cc06c2b2008459e3
Breno Santos <brenosantos145@gmail.com>
2022-06-02 18:35:08 -0300

    update

--- src/sequelize/migrations/20211020194502-create-user.js
+++ src/sequelize/migrations/20211020194502-create-user.js
@@ -26,7 +26,7 @@ module.exports = {
       },
     });
   },
-  down: async (queryInterface, Sequelize) => {
+  down: async (queryInterface, _Sequelize) => {
     await queryInterface.dropTable('Users');
   },
 };
