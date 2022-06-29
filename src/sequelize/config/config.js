f86f8ba045df3554074231b3bc12f5d2237adf72
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-06-29 17:56:04 -0300

    Adicionando dotenv

--- src/sequelize/config/config.js
+++ src/sequelize/config/config.js
@@ -1,23 +1,25 @@
+require('dotenv').config();
+
 module.exports = {
   development: {
-    username: 'root',
-    password: null,
-    database: 'database_development',
-    host: '127.0.0.1',
-    dialect: 'postgres',
+    username: process.env.DB_USER,
+    password: process.env.DB_PWD,
+    database: process.env.DATABASE,
+    host: process.env.HOST,
+    dialect: process.env.DIALECT,
   },
   test: {
-    username: 'root',
-    password: null,
-    database: 'database_test',
-    host: '127.0.0.1',
-    dialect: 'postgres',
+    username: process.env.DB_USER,
+    password: process.env.DB_PWD,
+    database: process.env.DATABASE,
+    host: process.env.HOST,
+    dialect: process.env.DIALECT,
   },
   production: {
-    username: 'root',
-    password: null,
-    database: 'database_test',
-    host: '127.0.0.1',
-    dialect: 'postgres',
+    username: process.env.DB_USER,
+    password: process.env.DB_PWD,
+    database: process.env.DATABASE,
+    host: process.env.HOST,
+    dialect: process.env.DIALECT,
   },
 };
