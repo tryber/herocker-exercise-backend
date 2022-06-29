926de5065c288452f11f24ae404a9d39aaad7959
Rafael Martins <rafamarts@gmail.com>
2022-06-28 22:57:38 -0300

    remove dialectOptions

--- src/sequelize/config/config.js
+++ src/sequelize/config/config.js
@@ -19,12 +19,6 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: 'postgres',
-    "dialectOptions": {
-      "ssl": {
-        "require": true,
-        "rejectUnauthorized": false
-      }
-    }
+    dialect: 'postgres'
   },
 };
