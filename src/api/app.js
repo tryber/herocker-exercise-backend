9a890ad2d33cb2dfa57d207c8b38d001e3576e19
Davíd Roggér <davidrogger.a@gmail.com>
2022-07-13 09:01:37 -0300

    install cors

--- src/api/app.js
+++ src/api/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const rescue = require('express-rescue');
+const cors = require('cors');
 const UserController = require('../controllers/user');
 const errorMiddleware = require('../middlewares/error');
 
 const app = express();
-
+app.use(cors());
 app.get('/users', rescue(UserController));
 
 app.use(errorMiddleware); 
