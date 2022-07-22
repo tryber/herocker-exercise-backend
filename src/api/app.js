e9184a71c470c6be754c1c87014a659bc480f467
fumagallilaura <la_12_msn@hotmail.com>
2022-07-22 18:12:30 -0300

    feat: add /users

--- src/api/app.js
+++ src/api/app.js
@@ -5,7 +5,7 @@ const errorMiddleware = require('../middlewares/error');
 
 const app = express();
 
-app.get('/', rescue(UserController));
+app.get('/users', rescue(UserController));
 
 app.use(errorMiddleware); 
 
