ea4ba0e37bc94717c936426181b004716c302040
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-09-05 09:44:51 -0300

    install and use cors

--- src/api/app.js
+++ src/api/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const rescue = require('express-rescue');
+var cors = require('cors');
 const UserController = require('../controllers/user');
 const errorMiddleware = require('../middlewares/error');
 
 const app = express();
+
+app.use(cors());
 
 app.get('/users', rescue(UserController));
 
